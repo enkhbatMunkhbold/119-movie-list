6248e3abd0037e8aeb7373cbc5beb5b2e6a3764b
Enkhbat Munkhbold <emunkhbold@Enkhbats-MacBook-Pro.local>
2019-06-21 17:16:55 -0700

    Implement basic event handler on react

--- src/app.jsx
+++ src/app.jsx
@@ -6,17 +6,29 @@
   --setup "Controlled Component"
     --store the text value for the input in React state
     --when new input is entered, first change it in the React state,
+    --when input text changes update the state
       then render the component based off that React state.
 --add functionality to handle search
 */
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { inputText: ''};
+        this.handleChange = this.handleChange.bind(this);
+    }
+    
+    handleChange(e) {
+        const inputText = e.target.value
+        this.setState({inputText});
+    }
     render () {
+        console.log(this.state.inputText)
         return (
             <div>
             <h1>Movie List</h1>
-                <input type="text" />
-                <button onClick={()=> console.log('Enkhbat')}>Search</button>
+                <input value={this.state.inputText} onChange={this.handleChange}type="text" />
+                <button onClick={()=> console.log(this.state.inputText)}>Search</button>
                 <ul>
                     {this.props.movies.map(movie => <li key={movie.id }>{movie.title} </li>)}
                 </ul> 
